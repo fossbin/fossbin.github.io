93b738b665aa67102a03a93550a7d77ca24f6a79
fossbin <stevedominicfez@gmail.com>
2025-06-27 12:52:08 +0530

    Comment out unused components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 import Hero from "@/components/sections/Hero";
-import Projects from "@/components/sections/Projects";
-import Contact from "@/components/sections/Contact";
 import React from 'react'
 
 export default function Home() {
