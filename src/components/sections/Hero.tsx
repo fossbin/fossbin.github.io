a4a7983f4e312cd62cc43b6e813f46f4be831558
fossbin <stevedominicfez@gmail.com>
2025-06-27 19:27:30 +0530

    Fine tune visibility on small displays

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -9,7 +9,7 @@ export default function Hero() {
           <div className="text-chart-2 text-3xl md:text-4xl font-bold mb-4">Hey !<span className="ml-1 animate-blink">|</span></div>
           <div className="text-4xl md:text-5xl font-bold mb-4">I'm Steve Fernandez. 👋</div>
           <p className="text-lg md:text-2xl font-normal mb-6">Passionate Full-Stack Developer who fueled by curiosity and coffee grounded shakes. I build scalable software and specialize with fullstack frameworks.</p>
-          <Button variant="reverse" className="bg-blue-200 font-bold hover:font-extrabold hover:underline hover:bg-blue-400"><a href="#contact">Get in Touch!
+          <Button variant="reverse" className="bg-blue-200 font-bold active:font-extrabold active:underline active:bg-blue-400 md:hover:font-extrabold md:hover:underline md:hover:bg-blue-400"><a href="#contact">Get in Touch!
           </a></Button>
         </div>
         <div className="flex-grow basis-2/5 ">
--- src/components/ui/marquee.tsx
+++ src/components/ui/marquee.tsx
@@ -1,22 +1,22 @@
 
 export default function Marquee({ items }: { items: {label: string, icon: React.ReactElement}[]; }) {
   return (
-    <div className="flex justify-center w-full bg-transparent p-8 h-[calc(70vh)] items-center relative overflow-hidden">
+    <div className="flex justify-center w-full bg-transparent p-8 h-[calc(50vh)] md:h-[calc(70vh)] items-center relative overflow-hidden">
       <>
           <div className="absolute top-10 right-20 w-12 h-12 bg-pink-500 border-4 border-black transform rotate-45"></div>
-          <div className="absolute bottom-20 left-16 w-8 h-16 bg-cyan-400 border-4 border-black transform -rotate-12"></div>
+          <div className="hidden md:block absolute bottom-20 left-16 w-8 h-16 bg-cyan-400 border-4 border-black transform -rotate-12"></div>
           <div className="absolute top-1/4 left-1/4 w-6 h-6 bg-yellow-400 border-2 border-black rounded-full"></div>
           <div className="absolute w-8 h-16 bottom-1/5 right-1/5 bg-emerald-600 border-4 border-black transform rotate-115 translate-x-8 translate-y-6"></div>
           <div className="relative top-1/5 left-1/3">
-            <div className="absolute w-14 h-20 bg-indigo-500 border-4 border-black transform rotate-15 translate-x-6 translate-y-4 shadow-[4px_4px_0px_0px_#000000]"></div>
-            <div className="absolute w-10 h-10 bg-cyan-400 border-4 border-black transform -rotate-30 translate-x-2 translate-y-12 shadow-[3px_3px_0px_0px_#000000]"></div>
+            <div className="absolute w-14 h-20 bg-indigo-500 border-4 border-black transform rotate-15 translate-x-6 translate-y-4 md:shadow-[4px_4px_0px_0px_#000000]"></div>
+            <div className="absolute w-10 h-10 bg-cyan-400 border-4 border-black transform -rotate-30 translate-x-2 translate-y-12 md:shadow-[3px_3px_0px_0px_#000000]"></div>
           </div>
 
       </>
       <div className="relative w-24 h-40">
-        <div className="absolute w-16 h-32 bg-red-500 border-4 border-black transform rotate-12 shadow-[6px_6px_0px_0px_#000000]"></div>
-            <div className="absolute w-12 h-24 bg-blue-600 border-4 border-black transform -rotate-6 translate-x-4 translate-y-2 shadow-[4px_4px_0px_0px_#000000]"></div>
-            <div className="absolute w-8 h-16 bg-yellow-400 border-4 border-black transform rotate-45 translate-x-8 translate-y-6 shadow-[3px_3px_0px_0px_#000000]"></div>
+        <div className="absolute w-16 h-32 bg-red-500 border-4 border-black transform rotate-12 md:shadow-[6px_6px_0px_0px_#000000]"></div>
+            <div className="absolute w-12 h-24 bg-blue-600 border-4 border-black transform -rotate-6 translate-x-4 translate-y-2 md:shadow-[4px_4px_0px_0px_#000000]"></div>
+            <div className="hidden md:block absolute w-8 h-16 bg-yellow-400 border-4 border-black transform rotate-45 translate-x-8 translate-y-6 shadow-[3px_3px_0px_0px_#000000]"></div>
             <div className="absolute w-6 h-6 bg-pink-500 border-2 border-black rounded-full transform translate-x-2 translate-y-8"></div>
             <div className="absolute w-4 h-4 bg-green-500 border-2 border-black transform rotate-45 translate-x-12 translate-y-12"></div>
       </div>
@@ -43,10 +43,10 @@ export default function Marquee({ items }: { items: {label: string, icon: React.
           </div>
         </div>
       <div className="relative w-24 h-40">
-        <div className="absolute w-20 h-28 bg-purple-600 border-4 border-black transform -rotate-8 shadow-[6px_6px_0px_0px_#000000]"></div>
-            <div className="absolute w-14 h-20 bg-orange-500 border-4 border-black transform rotate-15 translate-x-6 translate-y-4 shadow-[4px_4px_0px_0px_#000000]"></div>
-            <div className="absolute w-10 h-10 bg-cyan-400 border-4 border-black transform -rotate-30 translate-x-2 translate-y-12 shadow-[3px_3px_0px_0px_#000000]"></div>
-            <div className="absolute w-6 h-12 bg-lime-400 border-2 border-black transform rotate-60 translate-x-16 translate-y-8"></div>
+        <div className="absolute w-20 h-28 bg-purple-600 border-4 border-black transform -rotate-8 md:shadow-[6px_6px_0px_0px_#000000]"></div>
+            <div className="absolute w-14 h-20 bg-orange-500 border-4 border-black transform rotate-15 translate-x-6 translate-y-4 md:shadow-[4px_4px_0px_0px_#000000]"></div>
+            <div className="absolute w-10 h-10 bg-cyan-400 border-4 border-black transform -rotate-30 translate-x-2 translate-y-12 md:shadow-[3px_3px_0px_0px_#000000]"></div>
+            <div className="hidden md:block absolute w-6 h-12 bg-lime-400 border-2 border-black transform rotate-60 translate-x-16 translate-y-8"></div>
       </div>
     </div>
   )
